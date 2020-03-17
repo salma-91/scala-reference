09e13b9eacae2158ba977807b89a6b579371891d
selma zegdene <salma.zegdane@gmail.com>
2020-03-17 17:05:04 +0100

    add

--- src/main/scala/ClassesObjects.scala
+++ src/main/scala/ClassesObjects.scala
@@ -20,6 +20,9 @@ class Rational(n: Int, d: Int) {
 object ClassesObjects {
   def main(args: Array[String]): Unit = {
     var i = 1
+
+
+
     var x = new Rational(0, 1)
     while (i <= 10) {
       x += new Rational(1, i)
